abc4ab67ebd426cc9d7a2e5203ccfcc3831f6993
Håvard Lindset <havard@givn.no>
2024-11-24 20:48:06 +0100

    add percent scaling + params parser test

--- lib/imagex/transform/scale.ex
+++ lib/imagex/transform/scale.ex
@@ -4,25 +4,26 @@ defmodule Imagex.Transform.Scale do
   alias Imagex.TransformState
   alias Imagex.Transform.Scale.Parameters
 
-  def execute(%TransformState{image: image} = state, parameters) do
-    with {:ok, scale_params} <- Parameters.parse(parameters),
-         {:ok, scaled_image} <- do_scale(image, scale_params) do
+  def execute(%TransformState{} = state, parameters) do
+    with {:ok, parsed_params} <- Parameters.parse(parameters),
+         coord_mapped_params <- map_params_to_coords(state.image, parsed_params),
+         {:ok, scaled_image} <- do_scale(state.image, coord_mapped_params) do
       # reset focus to :center on scale
       %TransformState{state | image: scaled_image, focus: :center}
     end
   end
 
-  def do_scale(image, %Parameters{width: width, height: :auto}) do
+  def do_scale(image, %{width: width, height: :auto}) do
     scale = width / Image.width(image)
     Image.resize(image, scale)
   end
 
-  def do_scale(image, %Parameters{width: :auto, height: height}) do
+  def do_scale(image, %{width: :auto, height: height}) do
     scale = height / Image.height(image)
     Image.resize(image, scale)
   end
 
-  def do_scale(image, %Parameters{width: width, height: height}) do
+  def do_scale(image, %{width: width, height: height}) do
     width_scale = width / Image.width(image)
     height_scale = height / Image.height(image)
     Image.resize(image, width_scale, vertical_scale: height_scale)
@@ -30,5 +31,16 @@ defmodule Imagex.Transform.Scale do
 
   def do_scale(_image, parameters) do
     {:error, {:unhandled_scale_parameters, parameters}}
+  end
+
+  def to_coord(size, {:pct, pct}), do: round(size * pct / 100)
+  def to_coord(_size, {:int, int}), do: int
+  def to_coord(_size, :auto), do: :auto
+
+  def map_params_to_coords(image, %Parameters{width: width, height: height}) do
+    %{
+      width: to_coord(Image.width(image), width),
+      height: to_coord(Image.height(image), height)
+    }
   end
 end
--- lib/imagex/transform/scale/parameters.ex
+++ lib/imagex/transform/scale/parameters.ex
@@ -1,68 +1,88 @@
 #  int() <> "x" <> int() -> %{width: width, height: height}
-#    "*" <> "x" <> int() -> %{width: width, height: nil}
-#  int() <> "x" <> "*"   -> %{width: nil,   height: height}
-#  int()                 -> %{width: width, height: nil}
+#    "*" <> "x" <> int() -> %{width: :auto, height: height}
+#  int() <> "x" <> "*"   -> %{width: width, height: :auto}
+#  int()                 -> %{width: width, height: :auto}
 defmodule Imagex.Transform.Scale.Parameters do
   import NimbleParsec
 
   defstruct [:width, :height]
 
+  @type int_or_pct() :: {:int, integer()} | {:pct, integer()}
+
   @type t ::
           %__MODULE__{
-            width: integer() | :auto,
-            height: integer()
+            width: int_or_pct() | :auto,
+            height: int_or_pct()
           }
           | %__MODULE__{
-              width: integer(),
-              height: integer() | :auto
+              width: int_or_pct(),
+              height: int_or_pct() | :auto
             }
 
-  defcombinator(:auto_size, ignore(ascii_char([?*])))
-  defcombinator(:by, ignore(ascii_char([?x])))
+  percent_char = ascii_char([?p])
+  dot_char = ascii_char([?.])
+  auto_size_char = ascii_char([?*])
+  float = integer(min: 1) |> optional(dot_char |> ascii_string([?0..?9], min: 1))
+  integer = integer(min: 1) |> lookahead_not(choice([dot_char, percent_char]))
+
+  defcombinator(:int_size, integer |> unwrap_and_tag(:int))
+  defcombinator(:pct_size, float |> ignore(percent_char) |> tag(:pct))
+  defcombinator(:auto_size, ignore(auto_size_char) |> tag(:auto))
+
+  int_or_pct =
+    choice([
+      parsec(:int_size),
+      parsec(:pct_size)
+    ])
+
+  auto_width =
+    unwrap_and_tag(choice([int_or_pct, parsec(:auto_size)]), :width)
+    |> ignore(ascii_char([?x]))
+    |> unwrap_and_tag(int_or_pct, :height)
+
+  auto_height =
+    unwrap_and_tag(int_or_pct, :width)
+    |> ignore(ascii_char([?x]))
+    |> unwrap_and_tag(choice([int_or_pct, parsec(:auto_size)]), :height)
+
+  simple = unwrap_and_tag(int_or_pct, :width)
 
   defcombinator(
-    :explicit,
-    unwrap_and_tag(integer(min: 1), :width)
-    |> parsec(:by)
-    |> unwrap_and_tag(integer(min: 1), :height)
+    :dimensions,
+    choice([auto_width, auto_height, simple])
   )
-
-  defcombinator(
-    :auto_height,
-    unwrap_and_tag(integer(min: 1), :width) |> parsec(:by) |> parsec(:auto_size)
-  )
-
-  defcombinator(
-    :auto_width,
-    parsec(:auto_size) |> parsec(:by) |> unwrap_and_tag(integer(min: 1), :height)
-  )
-
-  defcombinator(:width, unwrap_and_tag(integer(min: 1), :width))
 
   defparsec(
     :internal_parse,
-    choice([
-      parsec(:explicit),
-      parsec(:auto_height),
-      parsec(:auto_width),
-      parsec(:width)
-    ])
+    parsec(:dimensions)
     |> eos()
   )
+
+  defp parse_number({:int, int}), do: {:int, int}
+  defp parse_number({:pct, [int]}), do: {:pct, int}
+
+  defp parse_number({:pct, [int_part, 46, decimal_part] = float_list}) do
+    case Float.parse("#{int_part}.#{decimal_part}") do
+      {float, _} -> {:pct, float}
+    end
+  end
 
   def parse(parameters) do
     case __MODULE__.internal_parse(parameters) do
       {:ok, [width: width], _, _, _, _} ->
-        {:ok, %__MODULE__{width: width, height: :auto}}
+        {:ok, %__MODULE__{width: parse_number(width), height: :auto}}
 
-      {:ok, [height: height], _, _, _, _} ->
-        {:ok, %__MODULE__{width: :auto, height: height}}
+      {:ok, [width: width, height: {:auto, _}], _, _, _, _} ->
+        {:ok, %__MODULE__{width: parse_number(width), height: :auto}}
+
+      {:ok, [width: {:auto, _}, height: height], _, _, _, _} ->
+        {:ok, %__MODULE__{width: :auto, height: parse_number(height)}}
 
       {:ok, [width: width, height: height], _, _, _, _} ->
-        {:ok, %__MODULE__{width: width, height: height}}
+        {:ok, %__MODULE__{width: parse_number(width), height: parse_number(height)}}
 
-      {:error, _, _, _, _, _} ->
-        {:error, :parameter_parse_error}
+      {:error, msg, _, _, _, _} ->
+        {:error, {:parameter_parse_error, msg, parameters}}
     end
   end
 end
