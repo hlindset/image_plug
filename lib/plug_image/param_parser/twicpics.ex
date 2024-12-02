d0812e7fe2710cb42c0c66f14b263e8b58c77319
Håvard Lindset <havard@givn.no>
2024-12-02 21:15:41 +0100

    merge to single reduce_while

--- lib/plug_image/param_parser/twicpics.ex
+++ lib/plug_image/param_parser/twicpics.ex
@@ -40,22 +40,24 @@ defmodule ImagePlug.ParamParser.Twicpics do
 
   def parse_chain(chain_str) do
       Regex.scan(@params_regex, chain_str, capture: :all_but_first)
-      |> Enum.reduce_while([], fn
-        [transform_name, params_str], acc when is_map_key(@transforms, transform_name) ->
-          {:cont, [{:ok, {Map.get(@transforms, transform_name), params_str}} | acc]}
+      |> Enum.reduce_while({:ok, []}, fn
+        [transform_name, params_str], {:ok, transforms_acc} when is_map_key(@transforms, transform_name) ->
+          module = Map.get(@transforms, transform_name)
+
+          case @parsers[module].parse(params_str) do
+            {:ok, parsed_params} ->
+              {:cont, {:ok, [{module, parsed_params} | transforms_acc]}}
+
+            {:error, {:parameter_parse_error, input}} ->
+              {:halt, {:error, {:invalid_params, {module, "invalid input: #{input}"}}}}
+          end
 
         [transform_name, _params_str], acc ->
           {:cont, [{:error, {:invalid_transform, transform_name}} | acc]}
       end)
-      |> Enum.reverse()
-      |> Enum.reduce_while({:ok, []}, fn {:ok, {module, params_str}}, {:ok, transforms_acc} ->
-      case @parsers[module].parse(params_str) do
-        {:ok, parsed_params} ->
-          {:cont, {:ok, transforms_acc ++ {module, parsed_params}}}
-
-        {:error, {:parameter_parse_error, input}} ->
-          {:halt, {:error, {:invalid_params, {module, "invalid input: #{input}"}}}}
+      |> case do
+        {:ok, transforms} -> {:ok, Enum.reverse(transforms)}
+        other -> other
       end
-      end)
   end
 end
